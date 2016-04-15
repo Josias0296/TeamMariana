1e04391346ba1b6b8f70929e889cfa1d6c7778da
roberto <rasstreze74@gmail.com>
2016-04-15 12:21:25 -0500

    script base de datos

---
+++ hackatonScript.sql
@@ -0,0 +1,44 @@
+create database directorio;
+go
+
+use directorio;
+go
+
+create table usuario(
+idUsuario int not null identity(1,1),
+nombreCompleto varchar(150) not null,
+usuario varchar(45) not null,
+contraseña varchar(45)not null,
+direccion varchar(255) not null,
+telefono varchar(10) not null,
+correoEslectronico varchar(100) not null,
+tipo_Usuario varchar(25) not null,
+CONSTRAINT pk_Usuario PRIMARY KEY (idUsuario),
+CONSTRAINT UQ_usuario UNIQUE(usuario)
+);
+go
+
+
+
+create table proveedores(
+idProvedor int not null identity(1,1),
+nombre varchar (200) not null,
+descripcion varchar(255) not null,
+telefono varchar(10) not null ,
+correoElectronico varchar(100) not null, 
+direccion varchar(255) not null, 
+CONSTRAINT pk_Proveedor PRIMARY KEY(idProvedor),
+CONSTRAINT UQ_Proveedor UNIQUE(nombre) 
+
+);
+go
+
+
+
+create table productos(
+idProducto int not null identity(1,1) primary key,
+nombre varchar(100) not null,
+descripcion varchar(255) not null,
+provedor_idProvedor int not null,
+ 
+
